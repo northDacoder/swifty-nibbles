f1988cf3e8cebae948b7322afcfece59677caec1
John Ryan <John.C.Ryan2@target.com>
2015-10-16 22:50:39 -0500

    initial commit

---
+++ swiftynibbles/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftynibbles
+//
+//  Created by John.C.Ryan2 on 10/16/15.
+//  Copyright © 2015 northdacoder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
